cbf264d53fc7d04be0e41325f471e575e8e7eb85
Danster2020 <daniel.bleckert@me.com>
2026-02-25 14:33:18 +0100

    .

--- project/rpicam.py
+++ project/rpicam.py
@@ -2,7 +2,7 @@ import cv2
 from picamera2 import Picamera2
 import time
 picam2 = Picamera2()
-picam2.configure(picam2.created_preview_configuration(raw={"size":(1640, 1232)}, main={"format":"RGB888", "size": (640, 480)}))
+picam2.configure(picam2.create_preview_configuration(raw={"size":(1640, 1232)}, main={"format":"RGB888", "size": (640, 480)}))
 picam2.start()
 time.sleep(2)
 
